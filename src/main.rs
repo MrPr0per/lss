24c552caf4b43a43f3b9f8897181fb6d98e813e3
g.host <73241824+MrPr0per@users.noreply.github.com>
2026-02-24 23:44:36 +0500

    upload update test

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+use chrono::Local;
 use std::env;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        eprintln!("Usage: lss <name>");
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    if args.is_empty() {
+        eprintln!("Usage: lss <args...>");
         return;
     }
-    println!("Hello, {}!", args[1]);
+    let now = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
+    const GREEN: &str = "\x1b[32m";
+    const RESET: &str = "\x1b[0m";
+    println!("{}{}{} {}", GREEN, now, RESET, args.join(" "));
 }
